daadb2ccd74cb5e625433f47f34de98474eb198b
Craig Arnush <ca_github@ghoti.net>
2023-03-28 20:04:45 +0100

    Add Contacts page

---
+++ contacts.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta name="viewport" content="width=device-width,initial-scale=1"/>
+
+    <!-- favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
+    <link rel="manifest" href="site.webmanifest">
+
+    <link rel="stylesheet" href="ArmadaCon.css" type="text/css">
+    <title>Contact Us</title>
+</head>
+
+<body>
+    <table class="ContactsTable">
+        <?php include("includes/social-media-icons.php")?>
+    </table>
+
+    <?php include("includes/countdown.php")?>
+
+    <!-- Main content section -->
+    <div class="content">
+        <h1 class="page-title">Contact Us</h1>
+
+        <h2>General Queries</h2>
+        <p>If you have a problem, if no one else can help, and if you can find them, then maybe you should contact the 'Eh?' Team.</p>
+        <p>
+            But seriously, if you have any questions, would like to put on an event or display at ArmadaCon,
+            or any other queries, then use the email address below and ask away.
+        </p>
+        <a style="margin: 40px" href="mailto:enquiries@armadacon.org">enquiries@armadacon.org</a>
+
+        <h2>Website Queries</h2>
+        <p>For website-related enquiries please e-mail the webmaster, putting 'ArmadaCon Website' in the subject line.</p>
+        <a style="margin: 40px" href="mailto:webmaster@armadacon.org?subject=ArmadaCon Website">webmaster@armadacon.org</a>
+
+        <h2>Snail Mail</h2>
+        <p>If you're a pen and paper sort of person, here is our contact postal address:</p>
+            <ul style="list-style-type: none; margin-bottom: 8px">
+                <li>ArmadaCon</li>
+                <li>23 The Square</li>
+                <li>Stonehouse</li>
+                <li>Plymouth</li>
+                <li>PL1 3JX</li>
+            </ul>
+    </div>
+
+    <?php include("includes/footer.php")?>
+</body>
+</html>
