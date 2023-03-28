36ceeacf45186ef670afdc9b4bbcbfda4ccbe68f
Craig Arnush <ca_github@ghoti.net>
2023-03-28 17:04:43 +0100

    Added Location page
     - CSS: New classes for the restaurant table
     - HTML: Added location.php
     - Images: Added Future Inns images

---
+++ location.php
@@ -0,0 +1,169 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta name="viewport" content="width=device-width,initial-scale=1"/>
+
+    <!-- favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
+    <link rel="manifest" href="site.webmanifest">
+
+    <link rel="stylesheet" href="ArmadaCon.css" type="text/css">
+    <title>Where is ArmadaCon?</title>
+</head>
+
+<body>
+    <table class="LocationTable">
+        <?php include("includes/social-media-icons.php")?>
+    </table>
+
+    <?php include("includes/countdown.php")?>
+
+    <!-- Main content section -->
+    <div class="content">
+        <p>&nbsp;</p>
+        <h1 class="page-title"><a href="http://www.futureinns.co.uk/plymouth/" target="_new">Future Inns, Plymouth</a></h1>
+        <p style="text-align: center">
+            <a href="http://www.futureinns.co.uk/plymouth/" target="_new">
+                <img alt="Future Inns Logo" src="Images/future-inns-logo.png" height="144" width="157" />
+            </a>
+            <a href="http://www.futureinns.co.uk/plymouth/" target="_new">
+                <img alt="Future Inns Plymouth building" src="Images/future-inns-plymouth.jpg" height="156" width="306" />
+            </a>
+        </p>
+
+        <h3>Rooms and Room Rates:</h3>
+        <p>
+            All rooms have either two Canadian double beds or one Canadian double bed &amp; one sofa bed. They are all
+            include an en-suite and are non-smoking.
+        </p>
+        <p>
+            ArmadaCon negotiates a special rate per room, per night based on one or two people sharing a standard room.
+            At the moment, the special rate has not yet been determined. Please check back here for updates.
+        </p>
+        <p>
+            Room rates do not include breakfast; that is an additional cost per person per night when pre-booked.
+            The cost is slightly higher if not pre-booked in advance.
+        </p>
+
+        <h3>How to Book a Room</h3>
+        <p>There are a number of ways to do this:</p>
+        <ul>
+            <li>You can email them, the address is: <a href="mailto:reception.plymouth@futureinns.co.uk">reception.plymouth@futureinns.co.uk</a></li>
+            <li>Their telephone number is: <a href="tel:+441752701000">+44 1752 701000</a></li>
+            <li>
+                You can write to them:
+                <ul style="list-style-type: none; margin-top: 8px; margin-bottom: 8px">
+                    <li>Future Inns</li>
+                    <li>1 William Prance Road</li>
+                    <li>Plymouth International Business Park</li>
+                    <li>Plymouth</li>
+                    <li>PL6 5ZD</li>
+                </ul>
+            </li>
+            <li>Or you can pay them a visit!</li>
+            <li>Alternatively, you can book directly via their website. Dependent on your length of stay and other factors; this <em>may</em> work out cheaper than the ArmadaCon room rate.</li>
+        </ul>
+
+        <h3>Directions &amp; Car Parking Information</h3>
+        <p>
+            You can find plenty of information on how to get to the hotel on their
+            <a href="https://www.futureinns.co.uk/plymouth/contact-directions/" target="_new">Contacts page</a>.
+        </p>
+        <p>
+            There are 2 main bus companies in Plymouth, whose buses can be used to get to the Future Inns. These are
+            <a href="http://www.plymouthbus.co.uk/timetables/" target="_new">Plymouth Citybus</a> and
+            <a href="https://www.stagecoachbus.com/timetables" target="_new">Stagecoach</a>. Find out more about their
+            timetables and closest bus stops by clicking through to their respective pages.
+        </p>
+        <p>
+            There is a large car park available. Note that you need to make sure you register your vehicle with the
+            hotel in order to avoid paying a parking fine. You can enter your vehicles registration plate
+            number at reception.
+        </p>
+
+        <h3>Facilities in area of Venue </h3>
+        <h4>Places to Eat:</h4>
+        <p>
+            In addition to the <a href="https://www.futureinns.co.uk/plymouth/restaurant/" target="_new">Fishbone Restaurant</a>
+            inside the Future Inns, Plymouth, there are a number of nearby places to eat.
+        </p>
+        <!--TODO It would be a good idea to get local copies of the restaurant logos. -->
+        <table class="restaurant-table">
+            <tr>
+                <th colspan="2">Place</th>
+                <th>Location</th>
+                <th>Distance</th>
+            </tr>
+            <tr>
+                <td class="restaurant-logo"><img alt="McDonald's Logo" src="https://www.mcdonalds.com/content/dam/sites/uk/nfl/icons/FooterLogo.jpg"/></td>
+                <td><a href="https://www.mcdonalds.com/gb/en-gb/location/plymouth/plymouth---tavistock-road/tavistock-road/8260729.html" target="_new">McDonald's</a></td>
+                <td><a href="https://www.google.co.uk/maps/place/McDonald's/@50.4145929,-4.1283457,17z/data=!4m15!1m8!3m7!1s0x486ced4031b43bed:0x8e8e1d95511ed419!2sTavistock+Rd,+Plymouth+PL6+5DA!3b1!8m2!3d50.4145929!4d-4.1261517!16s%2Fg%2F1tdq0kfw!3m5!1s0x486ced6ae9ee80a1:0xd8263d22370963cf!8m2!3d50.4141903!4d-4.1272583!16s%2Fg%2F1tfvscq2" target="_new">Tavistock Rd<br/>Plymouth<br/>PL6 5DA</a></td>
+                <td>0.2 miles</td>
+            </tr>
+            <tr>
+                <td class="restaurant-logo"><img alt="M&S Logo" src="https://asset1.cxnmarksandspencer.com/is/content/mands/M&S_LOGO"/></td>
+                <td><a href="https://www.marksandspencer.com/stores/crownhill-plymouth-sf-3515" target="_new">M&S Foodhall</a></td>
+                <td><a href="https://www.google.co.uk/maps/place/M%26S+Simply+Food/@50.4145929,-4.1283457,17z/data=!4m15!1m8!3m7!1s0x486ced4031b43bed:0x8e8e1d95511ed419!2sTavistock+Rd,+Plymouth+PL6+5DA!3b1!8m2!3d50.4145929!4d-4.1261517!16s%2Fg%2F1tdq0kfw!3m5!1s0x486ced6f9b5f176f:0xdf72c3d6ce5d883!8m2!3d50.416062!4d-4.1254953!16s%2Fg%2F1v_z3rxl" target="_new">Crownhill Retail Park<br/>4 Sendall's Way<br/>Plymouth<br/>PL6 5US</a></td>
+                <td>0.3 miles</td>
+            </tr>
+            <tr>
+                <td class="restaurant-logo"><img alt="KFC Logo" src="https://brand-uk.assets.kfc.co.uk/2022-09/kfc_head_logo.svg"/></td>
+                <td><a href="https://www.kfc.co.uk/kfc-near-me/plymouth-crown-hill-retail-park" target="_new">KFC</a></td>
+                <td><a href="https://www.google.co.uk/maps/place/KFC+Plymouth+-+Crown+Hill+Retail+Park/@50.4145929,-4.1283457,17z/data=!4m15!1m8!3m7!1s0x486ced4031b43bed:0x8e8e1d95511ed419!2sTavistock+Rd,+Plymouth+PL6+5DA!3b1!8m2!3d50.4145929!4d-4.1261517!16s%2Fg%2F1tdq0kfw!3m5!1s0x486cecfdc7ec6699:0xc02d0b2cde299079!8m2!3d50.4166108!4d-4.1243975!16s%2Fg%2F1v4psf16" target="_new">Crown Hill Retail Park<br/>227 Tavistock Road<br/>Crownhill Retail Park<br/>Plymouth<br/>PL6 5US</a></td>
+                <td>0.3 miles</td>
+            </tr>
+            <tr>
+                <td class="restaurant-logo"><img alt="Tim Hortons Logo" src="https://timhortons.co.uk/front/assets/img/core/logo-red.png"/></td>
+                <td><a href="https://timhortons.co.uk/" target="_new">Tim Hortons</a></td>
+                <td><a href="https://www.google.co.uk/maps/place/Tim+Hortons/@50.4155591,-4.1255095,17.25z/data=!4m15!1m8!3m7!1s0x486ced4031b43bed:0x8e8e1d95511ed419!2sTavistock+Rd,+Plymouth+PL6+5DA!3b1!8m2!3d50.4145929!4d-4.1261517!16s%2Fg%2F1tdq0kfw!3m5!1s0x486ced99d59948b3:0x3c671328a41760b8!8m2!3d50.4169493!4d-4.12401!16s%2Fg%2F11rhwhvwm8" target="_new">Crown Hill Retail Park<br/>Tavistock Rd<br/>Plymouth<br/>PL6 5US</a></td>
+                <td>0.4 miles</td>
+            </tr>
+            <tr>
+                <td class="restaurant-logo"><img alt="Jack Rabbit Logo" src="https://www.vintageinn.co.uk/content/dam/vintage-inn/images/pub-logos/thejackrabbitplymouth.png"/></td>
+                <td><a href="http://www.vintageinn.co.uk/restaurants/south-west/thejackrabbitplymouth/restaurantsfood" target="_new">The Jack Rabbit</a></td>
+                <td><a href="https://www.google.co.uk/maps/place/The+Jack+Rabbit/@50.4174879,-4.1217666,17.25z/data=!4m15!1m8!3m7!1s0x486ced4031b43bed:0x8e8e1d95511ed419!2sTavistock+Rd,+Plymouth+PL6+5DA!3b1!8m2!3d50.4145929!4d-4.1261517!16s%2Fg%2F1tdq0kfw!3m5!1s0x486ced14b87fbee7:0xca2a473957fe9325!8m2!3d50.4182897!4d-4.121256!16s%2Fg%2F1tj57629" target="_new">8 Howeson Ln<br/>Plymouth<br/>PL6 8BD</a></td>
+                <td>0.5 miles</td>
+            </tr>
+            <tr>
+                <td class="restaurant-logo"><img alt="Toby Carvery Logo" src="https://www.tobycarvery.co.uk/content/dam/toby-carvery/images/redesign/logo2.png"/></td>
+                <td><a href="https://www.tobycarvery.co.uk/restaurants/south-west/roboroughplymouth#/" target="_new">Toby Carvery</a></td>
+                <td><a href="https://www.google.co.uk/maps/place/Toby+Carvery+Plymouth/@50.4238963,-4.1178448,16.25z/data=!4m18!1m8!3m7!1s0x486ced4031b43bed:0x8e8e1d95511ed419!2sTavistock+Rd,+Plymouth+PL6+5DA!3b1!8m2!3d50.4145929!4d-4.1261517!16s%2Fg%2F1tdq0kfw!3m8!1s0x486ced6f02f871eb:0xe6a25fa1e28d7040!5m2!4m1!1i2!8m2!3d50.427836!4d-4.113296!16s%2Fg%2F1wfvm1_3" target="_new">399 Tavistock Rd<br/>Plymouth<br/>PL6 7HB</a></td>
+                <td>1.3 miles</td>
+            </tr>
+        </table>
+
+        <h3>Alternate Accommodation</h3>
+        <p>
+            On the same site as The Jack Rabbit, there is a
+            <a href="http://www.travelodge.co.uk/hotels/491/Plymouth-Derriford-hotel" target="_new">Travelodge (Plymouth Derriford)</a>
+        </p>
+
+        <h3>Additional Facilities</h3>
+        <p>
+            An, approximate, 15 minutes walk to the south will take you 0.6 miles to the &ldquo;village&rdquo; of Crownhill. Here
+            you will find a number of additional eateries and other amenities, including the following:</p>
+        <ul>
+            <li><em>Baba's 40 Dishes - Burgers Kebabs Wraps</em> takeaway</li>
+            <li><em>Boots</em> pharmacy</li>
+            <li><em>China Palace</em> Chinese takeaway</li>
+            <li><em>Co-op</em> food and grocery</li>
+            <li><em>Crown Spice</em> Indian takeaway</li>
+            <li><em>Crownhill Charcoal Grill</em> takeaway</li>
+            <li><em>Crownhill Chinese</em> takeaway</li>
+            <li><em>Nibana</em> Indian restaurant &amp; takeaway</li>
+            <li><em>Pizza King</em> takeaway</li>
+            <li>Post Office</li>
+            <li><em>Priority</em> dry cleaners</li>
+            <li><em>Stacked</em> sandwiches</li>
+            <li><em>The Tamar</em> pub</li>
+            <li><em>The Village Fish &amp; Chips</em> takeaway</li>
+        </ul>
+        <h3>&nbsp;</h3>
+    </div>
+
+    <?php include("includes/footer.php")?>
+</body>
+</html>
