f21f09c9033051beee24a53cd9892224b3fe6f7f
Craig Arnush <ca_github@ghoti.net>
2023-03-28 23:54:41 +0100

    Add Charity page

---
+++ charity.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <?php include("includes/html-header.php")?>
+    <title>Charity</title>
+</head>
+<body>
+    <?php
+        $page_name = "Charity";
+        include("includes/header-banner.php");
+    ?>
+
+    <!-- Main content section -->
+    <div class="content">
+        <div class="page-title">Charity</div>
+        <div style="text-align: center; font-size: large">
+            <img style="margin: 0; width: 800px" src="Images/cheque2019.jpg" alt="Giant Donation Cheque from 2019">
+            <p style="margin: 0 auto 0 auto">A giant cheque showing how much we raised from ArmadaCon 30.</p>
+            <p style="margin-top: 0">The total amount raised as of April 2019 is <strong>£9,104.50</strong></p>
+        </div>
+
+        <h2>St Luke's Hospice Plymouth</h2>
+        <p>
+            For many years ArmadaCon supported the RNIB Talking Books Service. To find out
+            more about the books that we have had transcribed, please have a look at the
+            <a href="http://www.armadacon.org/archive/Charity.html" target="_new">Charity page of the
+                ArmadaCon On-Line Archive</a>.
+        </p>
+        <p>
+            Due to some changes in the way RNIB works and the fact that we reached a
+            significant anniversary at ArmadaCon 25, the attendees were asked which charity they would like
+            to support, and St Luke's was chosen.
+        </p>
+        <div style="text-align: center">
+            <a href="https://www.stlukes-hospice.org.uk/home" target="_new">
+                <img src="Images/St Lukes Logo2.png" alt="St Luke's Logo"/>
+            </a>
+        </div>
+        <p>
+            St Luke&rsquo;s Hospice provides specialist end of life care to over 3,500
+            patients a year from across Plymouth, South West Devon and East Cornwall.
+            The aim of their teams is to provide the highest quality of care to alleviate
+            pain and distressing symptoms, offering practical, emotional and spiritual support to patients,
+            their families and their carers.
+        </p>
+        <p>They care for people with serious illnesses such as cancer, motor neurone
+            disease, heart failure, multiple sclerosis and chronic lung disease, treating any adult over the
+            age of 18. They do all they can to involve patients in decisions about their care, to maintain
+            privacy and dignity in providing that care and to respect patients&rsquo; individual rights and
+            religious and cultural beliefs. Their approach to care is holistic, centred on the needs of the
+            individual patient.
+        </p>
+        <p>
+            They have been offering their care to patients from across Plymouth, South West
+            Devon and East Cornwall, for over 30 years; wherever they need it most, whether in the home,
+            hospital or hospice and their teams of specialist doctors, nurses, occupational therapists and
+            social workers make this possible.
+        </p>
+    </div>
+
+    <?php include("includes/footer.php")?>
+</body>
+</html>
