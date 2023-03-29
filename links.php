ef46970462cb5fd589547a1e91d385184fb0bddb
Craig Arnush <ca_github@ghoti.net>
2023-03-29 02:12:16 +0100

    Add Links page
     - CSS: Add styles for the Links page
     - HTML: Add the Links page
     - HTML: Test all links to make sure they work

---
+++ links.php
@@ -0,0 +1,77 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <?php include("includes/html-header.php")?>
+    <title>Links</title>
+</head>
+<body>
+    <?php
+        $page_name = "Links";
+        include("includes/header-banner.php");
+    ?>
+
+    <!-- Main content section -->
+    <div class="content">
+        <div class="page-title">Links</div>
+        <table class="links-table">
+            <tr>
+                <td><a href="https://www.youtube.com/user/Guybo30?feature=guide" target="new">ArmadaCon YouTube Channel</a></td>
+                <td>ArmadaCon now has a YouTube Channel with videos from the Archive.</td>
+            </tr>
+            <tr>
+                <td><a href="http://www.plymouthwargamers.co.uk/" target="new">Plymouth Association of Wargamers</a></td>
+                <td>Plymouth Wargamers</td>
+            </tr>
+            <tr>
+                <td><a href="https://www.keep-it-secret.co.uk/" target="new">Keep It Secret</a></td>
+                <td>A local business specialising in Japanese and Asian pop culture.</td>
+            </tr>
+            <tr>
+                <td><a href="https://ukgeekcollective.weebly.com/index.html" target="new">Plymouth Geek Collective</a></td>
+                <td>An on-line community for geeks!</td>
+            </tr>
+            <tr>
+                <td><a href="https://plymouthmedievalsociety.com/index.htm" target="new">Plymouth Medieval Society</a></td>
+                <td>Medieval and Georgian period re-enactors based in Plymouth</td>
+            </tr>
+            <!--<tr>-->
+            <!--    <td><a href="https://smart-fx.co.uk/" target="new">Smart FX</a></td>-->
+            <!--    <td>This, Plymouth based, small business offers a variety of design &amp; illustration services; as well as art classes!</td>-->
+            <!--</tr>-->
+            <tr>
+                <td><a href="https://www.facebook.com/pages/Plymouth-United-Kingdom/Final-Frontier/113823418301?v=wall"
+                               target="new">Final Frontier Facebook Page</a></td>
+                <td>'Final Frontier' in Plymouth city centre</td>
+            </tr>
+            <tr>
+                <td><a href="https://www.facebook.com/Things-From-Dimension-X-148081071957620/" target="new">Things from Dimension - X Facebook Page</a></td>
+                <td>Based in Yeovil, this is a store for comics, figures and other collectables; also the writer of <em>The
+                    Titanic's Mummy</em></td>
+            </tr>
+            <!--<tr>-->
+            <!--    <td><a href="https://www.lowvisionart.org/index.htm" target="new">Low Vision Art</a></td>-->
+            <!--    <td>Website of Barrie Goodfellow, registered blind and deaf artist. Who spoke at and-->
+            <!--            donated a painting to ArmadaCon 22</td>-->
+            <!--</tr>-->
+            <!--<tr>-->
+            <!--    <td><a href="https://www.facebook.com/pages/Mr-Buns-Bakery/161143034754" target="new">Mr Buns / Lee Fox Smith</a></td>-->
+            <!--    <td>The creator of the masterpiece that was the ArmadaCon 20 cake!</td>-->
+            <!--</tr>-->
+            <tr>
+                <td><a href="https://www.visitplymouth.co.uk/" target="new">Visit Plymouth</a></td>
+                <td>A general site for details about our home city</td>
+            </tr>
+            <tr>
+                <td><a href="https://news.ansible.co.uk/" target="new">Ansible</a></td>
+                <td>The newsletter of Dave Langford; which includes convention listings</td>
+            </tr>
+            <tr>
+                <td><a href="https://genesis-sf.org.uk/" target="new">Genesis Sci-Fi Club</a></td>
+                <td>The Basingstoke Genesis Sci-Fi Club has a number of members who regularly attend ArmadaCon.</td>
+            </tr>
+        </table>
+    </div>
+
+    <?php include("includes/footer.php")?>
+</body>
+</html>
