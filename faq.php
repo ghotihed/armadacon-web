e918a590fd880bb2339593d702532205b0572750
Craig Arnush <ca_github@ghoti.net>
2023-03-29 01:06:16 +0100

    Add FAQ page
     - CSS: Add styles for the FAQ page
     - HTML: Add the FAQ page

---
+++ faq.php
@@ -0,0 +1,143 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <?php include("includes/html-header.php")?>
+    <title>FAQ</title>
+</head>
+<body>
+    <?php
+        $page_name = "FAQ";
+        include("includes/header-banner.php");
+    ?>
+
+    <!-- Main content section -->
+    <div class="content">
+        <div class="page-title">Frequently Asked Questions</div>
+
+        <div class="content-box">
+            <div class="faq-section-title">Conventions in General</div>
+            <div class="faq-question">What are conventions?</div>
+            <div class="faq-answer">
+                Conventions are organized events where like-minded people gather to interact with each other on a given subject.
+                The subject could be anything, Sci-Fi is only one subject of many types of convention that are held
+                across the world.
+            </div>
+            <div class="faq-question">When did conventions start?</div>
+            <div class="faq-answer">
+                The origins of conventions have been lost to the mists of time, but suffice to say they've been going for longer
+                than many people think. One popular notion is that conventions started with the advent of <em>Star
+                Trek</em> in the 1960's. This isn't true; they've been running since at least 1936, though some claim the first
+                was actually in 1891!
+            </div>
+            <div class="faq-question">How popular are conventions?</div>
+            <div class="faq-answer">
+                It's estimated that there's several conventions held every weekend somewhere in the world. Sci-fi conventions
+                are by far the most common.
+            </div>
+            <div class="faq-question">What happens at conventions?</div>
+            <div class="faq-answer">
+                In the early days, the likes of writers and film-makers would go to these conventions to swap ideas and discuss
+                various aspects of a given topic.
+                Since the advent of television, conventions have evolved radically. These events are now more fan orientated.
+                Conventions are opportunities for fans of a writer, film or TV series to meet the people who appear on the
+                silver screen or TV tube. What actually happens differs from convention to convention, but that sums it up in a
+                nutshell.
+            </div>
+            <div class="faq-question">Are conventions like I've seen in <em>Galaxy Quest</em> and <em>The Simpsons</em>?</div>
+            <div class="faq-answer">
+                Some are, but most aren't. Don't forget, these two shows were poking fun at conventions, so things were
+                exaggerated somewhat. Yes, some imagery and characters portrayed were accurate, but the numbers involved are
+                nowhere near the truth. Virtually all the people who go to conventions are normal, every day human beings.
+            </div>
+        </div>
+        <div class="content-box">
+            <div class="faq-section-title">Fans and Fandom</div>
+            <div class="faq-question">What is a fan?</div>
+            <div class="faq-answer">
+                A fan is a person who takes an interest in the work of another person or persons. This work could be anything
+                from watching plays written by Shakespeare, to following a given football team. Being a fan goes beyond simply
+                liking something. A fan of classical music will have many copies of the same piece, but performed by different
+                orchestras. A fan of a football team will often own the team strip or other items.
+            </div>
+            <div class="faq-question">What is fandom?</div>
+            <div class="faq-answer">
+                Fandom is the generic term for the fans of a given subject. For example; the football team Manchester United has
+                many fans, or Manchester United has its fandom. What is interesting to note, is that many TV Soaps have their
+                fans, but there's no predefined fandom for them.
+            </div>
+            <div class="faq-question">Can sci-fi fans tell the difference between fact and fantasy?</div>
+            <div class="faq-answer">
+                In a single word &quot;Yes&quot;, and they can do it better than most people. What you've heard is the
+                outpourings from the less intelligent areas of the media. This media will devote enormous amounts of time and
+                energy in reporting the latest gossip in the Soaps, then they'll poke fun at someone wearing a <em>Star
+                Trek</em> shirt!
+                Have you ever noticed throughout the years that fans of Soaps will send flowers when a <strong>character</strong> dies, or buy a
+                wedding special when two <strong>characters</strong> get married? The irony is that it's these same people that accuse sci-fi
+                fans of being unable to tell fact from fantasy!
+            </div>
+            <div class="faq-question">Do sci-fi fans dress up and pretend to be Captain Kirk?</div>
+            <div class="faq-answer">
+                Do children re-enact what they see on TV? Do football fans play football while wearing their favorite team strip
+                and pretend to be their favorite player? Do we all fantasize about being somebody else at times? Of course, we
+                do. Sci-fi fans are no different, so the answer to your question is yes. Mind you, please don't think that
+                <em>all</em> sci-fi fans like Captain Kirk; some dress up and take the mickey out of him!
+            </div>
+            <div class="faq-question">Why does the press portray fans as people who don't have a life?</div>
+            <div class="faq-answer">
+                The popular daily press doesn't care about people. They only want to sell papers. Therefore, they pick on
+                minority groups and hold them up to ridicule. Can you imagine what would happen if the papers mocked people who
+                watched <em>Eastenders</em> and <em>Coronation Street</em>? Yup, sales would plummet! Many fans have learned to avoid talking to
+                the popular press. In fact, some conventions even print warnings about the press in their literature!
+            </div>
+        </div>
+        <div class="content-box">
+            <div class="faq-section-title">ArmadaCon</div>
+            <div class="faq-question">What is ArmadaCon?</div>
+            <div class="faq-answer">
+                ArmadaCon is a sci-fi and fantasy multimedia convention that's been running in Plymouth since 1988.
+            </div>
+            <div class="faq-question">Why multimedia?</div>
+            <div class="faq-answer">
+                Other conventions tend to be focused one medium or one subject within that medium. We've found that fans find
+                this format restrictive, as most fans like more than one type of medium irrespective of subject.
+            </div>
+            <div class="faq-question">Why do you describe yourselves as 'unconventional'?</div>
+            <div class="faq-answer">
+                Many modern conventions have become very professional. They're slickly presented and well polished events. This
+                type of convention is considered by many to be the norm, due to large advertising budgets of the promoters.
+                Fans go to these conventions to be entertained by the guests.
+                ArmadaCon is completely different: a fan-run convention. Most attendees come to entertain each other!
+            </div>
+            <div class="faq-question">Fans entertaining each other? I don't understand.</div>
+            <div class="faq-answer">
+                It's true. Sci-fi fans are an outgoing bunch, and they don't take themselves too seriously. ArmadaCon provides
+                the opportunity for these people to put on events that might never see the light of day anywhere else.
+            </div>
+            <div class="faq-question">You don't seem to have any big names as guests, why?</div>
+            <div class="faq-answer">
+                We've found that they're not as interesting as the minor league ones. The more well known people have told their
+                stories many times over, so they don't come across as well. Since ArmadaCon is a multimedia convention, we
+                like to have guests from behind the camera as well as writers etc.
+            </div>
+            <div class="faq-question">Your ticket price is more expensive than other conventions. Why's that?</div>
+            <div class="faq-answer">
+                Don't be fooled by events that have 'cheap' ticket prices. They have hidden extras that we don't. Our guests don't
+                usually charge for autographs. Our guests aren't hidden away in a 'Green Room'. It's perfectly normal for our
+                guests to be having informal chats with the attendees in the bar, etc. With that level of access, we actually
+                work out cheaper!
+            </div>
+            <!--
+            <div class="faq-question">How come I've not heard of ArmadaCon before?</div>
+            <div class="faq-answer">
+                When did you last see an advert for Rolls-Royce cars? Chances are that you never have. Good things
+                tend to advertise themselves. ArmadaCon works in much the same way, that's by word of mouth. Don't
+                get us wrong, we do advertise, but it's selective. Because of our venue, we have to limit the numbers
+                that we can take.
+            </div>
+            -->
+        </div>
+    </div>
+
+    <?php include("includes/footer.php")?>
+</body>
+</html>
