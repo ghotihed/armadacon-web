e748c2b0c695ea39150d3f2221f131106fef6951
Craig Arnush <ca_github@ghoti.net>
2023-03-29 02:24:31 +0100

    Rearranged sitemap footer

--- includes/footer.php
+++ includes/footer.php
@@ -20,28 +20,25 @@
     <table>
         <tr>
             <td><a href="index.php">Home</a></td>
-            <td><a href="about.php">What is ArmadaCon?</a></td>
-            <td><a href="charity.php">Charity</a></td>
-            <!--<p><a href="https://www.armadacon.org/forum/" target="new">ArmadaCon Forum</a></p>-->
-            <td><a href="https://www.armadacon.org/archive2020/" target="new">ArmadaCon Archive</a></td>
-        </tr>
-        <tr>
-            <td><a href="news.php">News</a></td>
-            <td><a href="location.php">Where is ArmadaCon?</a></td>
-            <td><a href="local-events.php">Local Events</a></td>
+            <td><a href="registration.php">Tickets / Register</a></td>
             <td><a href="links.php">Links</a></td>
         </tr>
         <tr>
-            <td><a href="guests.php">Guests</a></td>
-            <td><a href="faq.php">FAQ</a></td>
-            <td><a href="contacts.php">Contact Us</a></td>
-            <!--<td>&nbsp;</td>-->
+            <td><a href="about.php">What is ArmadaCon?</a></td>
+            <td><a href="policies.php">Policies</a></td>
+            <td><a href="https://www.armadacon.org/archive2020/" target="new">ArmadaCon Archive</a></td>
         </tr>
         <tr>
-            <td><a href="registration.php">Tickets / Register</a></td>
-            <!--<td>&nbsp;</td>-->
-            <td><a href="policies.php">Policies</a></td>
-            <!--<td>&nbsp;</td>-->
+            <td><a href="location.php">Where is ArmadaCon?</a></td>
+            <td><a href="faq.php">FAQ</a></td>
+            <td><a href="contacts.php">Contact Us</a></td>
+        </tr>
+        <tr>
+            <td><a href="guests.php">Guests</a></td>
+            <td><a href="charity.php">Charity</a></td>
+            <!--<p><a href="https://www.armadacon.org/forum/" target="new">ArmadaCon Forum</a></p>-->
+            <!--<td><a href="news.php">News</a></td>-->
+            <!--<td><a href="local-events.php">Local Events</a></td>-->
         </tr>
     </table>
 </div>
