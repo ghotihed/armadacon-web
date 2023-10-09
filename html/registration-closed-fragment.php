bd892068edeb7b0593bcf168c52c6d409934342f
Craig Arnush <ca_github@ghoti.net>
2023-10-09 17:34:22 +0100

    Clarify contents of registration-closed fragment

--- html/registration-closed-fragment.php
+++ html/registration-closed-fragment.php
@@ -1,4 +1,7 @@
 <p>
-    Pre-registration for ArmadaCon <?= $current_year ?> is now closed. Registration for
-    next year's convention will open during this year's convention.
+    Pre-registration for ArmadaCon <?= $current_year ?> is now closed. You are still welcome
+    to register at the door. Just turn up, and we'll be happy to have you!
+</p>
+<p>
+    Registration for next year's ArmadaCon <?=$current_year + 1?> convention will open during this year's convention.
 </p>
