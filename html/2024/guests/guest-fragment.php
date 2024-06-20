e3fe72f66b7f1b3653784ee8441ed893c23fa488
Craig Arnush <ca_github@ghoti.net>
2024-06-20 15:01:55 +0100

    Update main page with the newly listed guests.

--- html/2024/guests/guest-fragment.php
+++ html/2024/guests/guest-fragment.php
@@ -1,23 +1,10 @@
 <?php
     global $convention;
-    if (false) {
-        echo <<<FULL_GUESTS
-            <p>
-                This year we will have <a href="<?={$convention->year()}?>/guests#jaine-fenn">Jaine Fenn</a>,
-                <a href="<?={$convention->year()}?>/guests#david-giron">David Fern&aacute;ndez Gir&oacute;n</a>,
-                and <a href="<?={$convention->year()}?>/guests#charlotte-merrill">Charlotte Merrill</a>. Full details of
-                all our guests are available on the <a href="<?={$convention->year()}?>/guests">guests page</a>.
-            </p>
-        FULL_GUESTS;
-    } else {
-        echo <<<TWO_GUESTS
-            <p>
-                This year we have booked a number of guests. Our first two are
-                <a href="{$convention->year()}/guests#jaine-fenn">Jaine Fenn</a> and
-                <a href="{$convention->year()}/guests#dominic-glynn">Dominic Glynn</a>.
-                Others will be announced at later dates, so stay tuned!
-                Full details of all our announced guests are available on the <a href="{$convention->year()}/guests">guests page</a>.
-            </p>
-        TWO_GUESTS;
-    }
 ?>
+<p>
+    This year we will have <a href="<?=$convention->year()?>/guests#jaine-fenn">Jaine Fenn</a>,
+    <a href="<?=$convention->year()?>/guests#dominic-glynn">Dominic Glynn</a>,
+    <a href="<?=$convention->year()?>/guests#david-giron">David Fern&aacute;ndez Gir&oacute;n</a>,
+    and <a href="<?=$convention->year()?>/guests#charlotte-merrill">Charlotte Merrill</a>. Full details of
+    all our guests are available on the <a href="<?=$convention->year()?>/guests">guests page</a>.
+</p>
