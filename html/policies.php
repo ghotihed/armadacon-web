7df7766e822c84f9e0a2d29c15122fc9842b087b
Craig Arnush <ca_github@ghoti.net>
2024-09-28 17:17:40 +0100

    Update Code of Conduct policies

--- html/policies.php
+++ html/policies.php
@@ -17,187 +17,137 @@
         <p>
             There are a number of rules and information you should be aware of when attending ArmadaCon.
         </p>
-        <ol>
-            <li>
-                Please wear your badge at all times, in plain sight; you need it to access convention spaces.
+        <ul>
+            <li>Please wear your badge at all times, in plain sight; you need it to access convention spaces.</li>
+            <li>Bear in mind that we're a family-friendly event; there will be children around. Keep it PG-13.</li>
+            <li>You are responsible for your property and personal safety. We're not responsible for lost, stolen or
+                damaged property, or for injuries sustained during the convention.
             </li>
-            <li>
-                Bear in mind that we're a family-friendly event; there will be children around. Keep it PG-13.
-            </li>
-            <li>
-                We're not responsible for lost, stolen or damaged property, or for injuries sustained during the
-                convention. We can offer sympathy though.
-            </li>
-            <li>
-                We reserve the right to ask attendees to leave the convention, without refund, if they are behaving
+            <li>We reserve the right to ask attendees to leave the convention, without refund, if they are behaving
                 inappropriately.
             </li>
-            <li>
-                Events and guests are subject to change and cancellation. We'll do our best to warn you of changes via our
-                website, Twitter, and Facebook, but sometimes changes will happen without notice.
+            <li>Events and guests are subject to change and cancellation. We'll do our best to warn you of changes via
+                our website and Facebook, but sometimes changes will happen without notice.
             </li>
-            <li>
-                We love photos, but please ask your subjects for their permission first - it's only polite. Also, please
+            <li>We love photos, but please ask your subjects for their permission first - it's only polite. Also, please
                 be aware of the traffic around and do not block the corridors for extended periods. Please do not block
                 doors at any time.
             </li>
-            <li>
-                Under-13s must be accompanied by their adult at all times. We want children to enjoy themselves, but we
+            <li>Under-13s must be accompanied by their adult at all times. We want children to enjoy themselves, but we
                 are not a crèche.
             </li>
-            <li>
-                Assistance animals are permitted in all public areas and at all times. No other animals will be permitted
-                in the hotel.
+            <li>Children and young adults are subject to convention policies and people acting as their responsible
+                adults are accountable for the conduct of their charges
             </li>
-            <li>
-                The hotel and convention rooms are a public space, and it is therefore illegal to smoke indoors. If you
-                want to smoke, the hotel has a shelter in the car park for that purpose. This also includes e-cigarettes and
-                similar equipment which the hotel classes as smoking.
+            <li>No animals except assistance animals are allowed in the hotel. Members with assistance animals are
+                reminded that they are responsible for the conduct of their assistance animal. All members are reminded
+                that assistance animals are working and should not be disturbed whilst going about their duties.
             </li>
-            <li>
-                If you have any problems or requests with regard to the convention, your first contact point is the
+            <li>Smoking, vaping etc. is not permitted within the hotel. If you want to smoke, the hotel has a shelter in
+                the car park for that purpose.
+            </li>
+            <li>If you have any problems or requests with regard to the convention, your first contact point is the
                 convention Registration Desk. If you have any problems or requests with regard to the hotel, your first
                 point of contact is the hotel Reception Desk.
             </li>
-        </ol>
+        </ul>
+
+        <p>Members are reminded that whilst at ArmadaCon they are also subject to the rules and policies of the venue
+            Hotel and Website Terms & Conditions - Future Inns Hotels</p>
 
         <h2>Anti-Harassment Policy</h2>
-        <p>
-            ArmadaCon is dedicated to providing a great convention experience for everyone, whatever their gender,
-            sexuality, disability, physical appearance, body size, race or religion. For this reason, we will not
-            tolerate <em>any</em> form of harassment of convention participants. Con participants found to be engaging
-            in harassment may be sanctioned or expelled from the convention without refund.
-        </p>
-        <p>
-            Harassment can include (but is not limited to):
-        </p>
+        <p>ArmadaCon is dedicated to providing a great convention experience for everyone, whatever their gender,
+            sexuality, disability, physical appearance, body size, race, religion, or other differences. We will not
+            tolerate any form of harassment of convention participants. Con participants found to be engaging in
+            harassment may be sanctioned or expelled from the convention without refund.</p>
+
+        <p>Harassment can include (but is not limited to):</p>
         <ul>
-            <li>
-                offensive verbal comments about gender, sexuality, impairment, physical appearance, body size, race or
+            <li>Offensive verbal comments about gender, sexuality, impairment, physical appearance, body size, race or
                 religion
             </li>
-            <li>showing sexual imagery in public spaces</li>
-            <li>intimidation, stalking or following</li>
-            <li>photographing/recording someone without their permission</li>
-            <li>sustained disruption of talks or other events</li>
-            <li>uninvited physical contact or sexual attention</li>
+            <li>Showing sexual imagery in public spaces</li>
+            <li>Intimidation, stalking or following</li>
+            <li>Photographing/recording someone without their permission</li>
+            <li>Sustained disruption of talks or other events</li>
+            <li>Uninvited physical contact or sexual attention</li>
         </ul>
 
         <h3>What should I do if I am being harassed?</h3>
-
-        <p>
-            In some cases you may find the harassment stops if you clearly say 'no', 'please leave me alone' or simply
+        <p>In some cases you may find the harassment stops if you clearly say 'no', 'please leave me alone' or simply
             walk away. We would appreciate it if the registration desk was informed, to help us identify any repeat
-            offenders.
-        </p>
-        <p>
-            If you continue to be harassed or notice someone else being harassed; please visit the registration desk or
+            offenders.</p>
+
+        <p>If you continue to be harassed or notice someone else being harassed; please visit the registration desk or
             contact a member of the convention committee. Committee members can help participants contact the hotel or
-            law enforcement (if that&#39;s what they&#39;d like to do), provide a listener, or otherwise assist those
-            experiencing harassment to feel safe during the Convention.
-        </p>
+            law enforcement (if that's what they'd like to do), provide a listener, or otherwise assist those
+            experiencing harassment to feel safe during the Convention.</p>
 
-        <h2 class="BodyText">Costumes and Weapons Policy</h2>
-        <p>
-            We love costumes at ArmadaCon, and are very much looking forward to seeing all your amazing cosplay! Here
-            are the cosplay rules to help keep things fun and safe for everyone.
-        </p>
-        <uL>
-            <li>
-                <strong>NO REAL</strong> weapons may be carried or worn except as part of a costume in the Masquerade and its
-                sub-categories or as part of an authorized display or panel.
-            </li>
-            <li>
-                No weapons should be carried or brandished off the Convention Premises unless they are obviously not real.
-                As the hotel is a public place, we would ask you to consider the effect that carrying a weapon may have one
-                the other patrons.
-            </li>
-            <li>
-                Weapons may be displayed (with consideration for others) and you can have your “my weapon is bigger than
-                yours” contest in the Main Hall and Games/Model Room, but must be stowed away safely at all times whilst in
-                public areas. We know they are not real; a member of the public may not.
-            </li>
-            <li>
-                The Committee reserves the right to confiscate any weapon not complying with the above and remove from the
-                convention any attendee consistently ignoring this policy.
-            </li>
-        </uL>
-
-        <h2 class="BodyText">Courtesy Policy</h2>
-        <p>
-            ArmadaCon hopes all our attendees abide by our Courtesy Policy, which is the exhibition of courteous
-            behaviour to fellow attendees.
-        </p>
+        <h2>Costumes and Weapons Policy</h2>
+        <p>We love costumes at ArmadaCon, and are very much looking forward to seeing all your amazing cosplay! Here are
+            the cosplay rules to help keep things fun and safe for everyone.</p>
         <ul>
-            <li>
-                Phones and other electronic devices should be silenced or turned off while in the panels and video rooms.
-                You might as well as reception is rubbish in the convention rooms.
+            <li><b>NO REAL</b> weapons may be carried or worn except as part of a costume in the Masquerade and its
+                sub-categories or as part of an authorised display or panel.
             </li>
-            <li>
-                Please don’t have a private conversation during the panels. If you want to chat feel free to sit in the
+            <li>No weapons whether real, replica, or imitation should be carried or brandished especially outside of
+                convention-only spaces unless they are obviously not real. We would ask you to consider the effect that
+                carrying a weapon,or item that may be mistaken for a weapon might have on the other patrons. You may
+                know that it’s not real but a member of the public might not and a visit from police officers with very
+                real firearms will spoil everyone’s day.
+            </li>
+            <li>The Committee reserves the right to request the removal of any item they feel is not complying with the
+                above and remove from the convention any attendee consistently ignoring this policy.
+            </li>
+        </ul>
+
+        <h2>Courtesy</h2>
+        <p>ArmadaCon expects all our members to be polite, respectful and considerate to each other this includes but is
+            not limited to:</p>
+        <ul>
+            <li>Phones and other electronic devices should be silenced or turned off while in the panels and video
+                rooms.
+            </li>
+            <li>Please don’t have a private conversation during the panels. If you want to chat feel free to sit in the
                 bar or lobby.
             </li>
-            <li>
-                Parents with small children, who misbehave, cry or become noisy during a panel, while we don’t want to
-                spoil the convention for you, please remember there are others in the room as well who might be disturbed.
-                It would be appreciated if you could leave the room as soon as possible.
+            <li>Parents with small children, who misbehave, cry or become noisy during a panel, while we don’t want to
+                spoil the convention for you, please remember there are others in the room as well who might be
+                disturbed. It would be appreciated if you could leave the room as soon as possible.
             </li>
-            <li>
-                Attendees wearing hats or tall costumes should remove them when sitting in a panel or sit at the back so
+            <li>Attendees wearing hats or tall costumes should remove them when sitting in a panel or sit at the back so
                 the view of other attendees is not blocked.
             </li>
-            <li>
-                When the main hall doors have been closed due to hallway noise, attendees are asked to open and close the
-                doors quietly.
+            <li>When the main hall doors have been closed due to hallway noise, attendees are asked to open and close
+                the doors quietly.
             </li>
-            <li>
-                Attendees immediately outside the doors to the main hall must keep their voices at a reasonable level so
+            <li>Attendees immediately outside the doors to the main hall must keep their voices at a reasonable level so
                 as not to disturb the programme.
             </li>
-            <li>
-                Attendees are asked to respect other attendees by not disrupting or intruding in their private
+            <li>Attendees are asked to respect other attendees by not disrupting or intruding in their private
                 conversations, videotaping/photography sessions, podcasting/recording sessions, or other activities.
             </li>
         </ul>
 
-        <h2>Hygiene Concept</h2>
-        <p>
-            As we are still mindful of returning after Covid first hit. We aim to make ArmadaCon as safe and
-            relaxed as possible. As of now, there are no formal restrictions in England.
-            With that in mind, please be aware of the following:
-        </p>
+
+        <h2>Hygiene and disease mitigation</h2>
+        <p>Although the lockdowns of 2020 and 2021 are thankfully well behind us Covid-19 is still an issue especially
+            when, like ArmadaCon, there is a gathering of people coming from across the country. Even if it wasn’t,
+            nobody wants to go home with “con-crud” as an unwelcome souvenir. So, with that in mind, please be aware of
+            the following:</p>
         <ul>
-            <li>
-                Mask-wearing is now your personal choice, but please be mindful and respect everyone who wishes to do
-                so.
+            <li>Mask-wearing is now your personal choice, but please be mindful and respect everyone who wishes to do so
+                or chooses not to.
             </li>
-            <li>
-                We will have hand sanitiser and a limited supply of masks available at the reg-desk.
-            </li>
-            <li>
-                Of course, good old-fashioned hand-washing is always encouraged!
-            </li>
-            <li>
-                We are asking people to be respectful of other attendees by keeping your distance where
-                possible and being aware of personal space.
-            </li>
-            <li>
-                We also encourage you to take a lateral flow test before coming to the con. This will ease the mind of all
+            <li>We encourage you to take a lateral flow test before coming to the con. This is for the benefit of all
                 attendees, especially the more vulnerable amongst us.
             </li>
-            <li>
-                Please do not attend if the test is positive, you have COVID-19 symptoms or are feeling unwell.
-            </li>
+            <li>Please do not attend if the test is positive, you have COVID-19 symptoms or are feeling unwell.</li>
         </ul>
 
-        <h3>
-            ArmadaCon want to make the convention as accessible to as many people as possible no matter
-            what their situation.
-        </h3>
-        <h3>
-            Being a small convention, we are very adaptable; so please let us know if there is something
-            we can do to help, and we will do our best to arrange it.
-        </h3>
+        <hr/>
+        <p style="font-size: larger; font-weight: bold;">Being a small convention, we are very adaptable; so please let us know if there is something we can do to
+            help, and we will do our best to arrange it.</p>
     </div>
 
     <?php include("includes/footer.php")?>
