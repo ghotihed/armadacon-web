05c472d1be24c715300e78f5ce5730165c9cf3e4
Craig Arnush <ca_github@ghoti.net>
2024-09-29 16:07:54 +0100

    Add 2024 programme

--- html/2024/programme/index.php
+++ html/2024/programme/index.php
@@ -2,9 +2,10 @@
 <html lang="en">
 
 <?php
+global $convention;
 $page_name = "home";
-$page_title = "ArmadaCon 2024 Programme";
-include(__DIR__ . "/../../includes/html-header.php")
+$page_title = "ArmadaCon Programme";
+include(__DIR__ . "/../../includes/html-header.php");
 ?>
 
 <body>
@@ -12,12 +13,42 @@ include(__DIR__ . "/../../includes/html-header.php")
 
 <!-- Main content section -->
 <div class="content">
-    <h1 class="page-title">ArmadaCon 2024 Programme</h1>
+    <h1 class="page-title">ArmadaCon <?=$convention->year()?> Programme</h1>
 
-    <div class="content-box">
-        The programme for any convention usually doesn't get finalised until very soon before the convention
-        itself. ArmadaCon is no different. Expect to see things here about a month before the convention.
-    </div>
+    <?php
+        $json_data = false;
+        if (file_exists(__DIR__ . "/programme.json")) {
+            $json = file_get_contents(__DIR__ . "/programme.json");
+            if ($json === false) {
+                $json_data = false;
+            } else {
+                $json_data = json_decode($json, true);
+            }
+            if ($json_data !== false) {
+                foreach ($json_data['days'] as $day) {
+                    $day_title = $day['title'];
+                    $day_note = $day['note'];
+                    echo '<div class="content-box">';
+                    echo '<div class="programme-day-title">' . $day_title . '</div>';
+                    foreach ($day['items'] as $item) {
+                        $item_title = $item['title'];
+                        $item_description = $item['description'];
+                        $item_start = $item['start'];
+                        $item_end = $item['end'];
+                        echo '<div class="programme-slot-title">' . $item_start . ' - ' . $item_end . ' ' . $item_title . '</div>';
+                        echo '<div class="programme-slot-description">' . $item_description . '</div>';
+                    }
+                    if (trim($day_note) !== '') {
+                        echo '<div class="programme-note"><b>Please note:</b> ' . $day_note . '</div>';
+                    }
+                    echo '</div>';
+                }
+            }
+        }
+        if ($json_data === false) {
+            echo "<div class='content-box'>The programme for any convention usually doesn't get finalised until very soon before the convention itself. ArmadaCon is no different. Expect to see things here about a month before the convention.</div>";
+        }
+    ?>
 </div>
 
 <?php include(__DIR__ . "/../../includes/footer.php")?>
