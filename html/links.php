cf36373de9c280f02893cdf35b28012f9c59a292
Craig Arnush <ca_github@ghoti.net>
2024-12-16 02:25:40 +0000

    Add Board Game Conventions List link

--- html/links.php
+++ html/links.php
@@ -22,10 +22,6 @@
                 <td><a href="http://www.plymouthwargamers.co.uk/" target="new">Plymouth Association of Wargamers</a></td>
                 <td>Plymouth Wargamers</td>
             </tr>
-<!--            <tr>-->
-<!--                <td><a href="https://www.keep-it-secret.co.uk/" target="new">Keep It Secret</a></td>-->
-<!--                <td>A local business specialising in Japanese and Asian pop culture.</td>-->
-<!--            </tr>-->
             <tr>
                 <td><a href="https://ukgeekcollective.weebly.com/index.html" target="new">Plymouth Geek Collective</a></td>
                 <td>An on-line community for geeks!</td>
@@ -34,10 +30,6 @@
                 <td><a href="https://plymouthmedievalsociety.com/index.htm" target="new">Plymouth Medieval Society</a></td>
                 <td>Medieval and Georgian period re-enactors based in Plymouth</td>
             </tr>
-            <!--<tr>-->
-            <!--    <td><a href="https://smart-fx.co.uk/" target="new">Smart FX</a></td>-->
-            <!--    <td>This, Plymouth based, small business offers a variety of design &amp; illustration services; as well as art classes!</td>-->
-            <!--</tr>-->
             <tr>
                 <td><a href="https://www.facebook.com/pages/Plymouth-United-Kingdom/Final-Frontier/113823418301?v=wall"
                                target="new">Final Frontier Facebook Page</a></td>
@@ -48,15 +40,6 @@
                 <td>Based in Yeovil, this is a store for comics, figures and other collectables; also the writer of <em>The
                     Titanic's Mummy</em></td>
             </tr>
-            <!--<tr>-->
-            <!--    <td><a href="https://www.lowvisionart.org/index.htm" target="new">Low Vision Art</a></td>-->
-            <!--    <td>Website of Barrie Goodfellow, registered blind and deaf artist. Who spoke at and-->
-            <!--            donated a painting to ArmadaCon 22</td>-->
-            <!--</tr>-->
-            <!--<tr>-->
-            <!--    <td><a href="https://www.facebook.com/pages/Mr-Buns-Bakery/161143034754" target="new">Mr Buns / Lee Fox Smith</a></td>-->
-            <!--    <td>The creator of the masterpiece that was the ArmadaCon 20 cake!</td>-->
-            <!--</tr>-->
             <tr>
                 <td><a href="https://www.visitplymouth.co.uk/" target="new">Visit Plymouth</a></td>
                 <td>A general site for details about our home city</td>
@@ -68,6 +51,10 @@
             <tr>
                 <td><a href="https://genesis-sf.org.uk/" target="new">Genesis Sci-Fi Club</a></td>
                 <td>The Basingstoke Genesis Sci-Fi Club has a number of members who regularly attend ArmadaCon.</td>
+            </tr>
+            <tr>
+                <td><a href="https://boardgamegeek.com/wiki/page/Game_Conventions">Board Game Conventions List</a></td>
+                <td>A list of board game conventions from around the world.</td>
             </tr>
         </table>
     </div>
